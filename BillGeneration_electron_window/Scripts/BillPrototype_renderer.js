9b0bda79ec2ad7b098612af641cfe538c9806ee2
Shantanu Methikar <smethikar18@gmail.com>
2020-03-25 13:17:42 +0530

    first prototype of bill ready

---
+++ BillGeneration_electron_window/Scripts/BillPrototype_renderer.js
@@ -0,0 +1,38 @@
+const CompanyName = document.getElementById('companyName');
+CompanyName.innerHTML = "Name of the Company";
+
+const Address = document.getElementById('address');
+Address.innerHTML = "Apartment number, Building Name, Society, Area, City - 000 000";
+
+const EmailID = document.getElementById('emailID');
+EmailID.innerHTML = "company@domain.com";
+
+const Contact = document.getElementById('contact');
+Contact.innerHTML = "(+11) 1234567890";
+
+const BuyerName = document.getElementById('buyerName');
+BuyerName.innerHTML = "Name of the Buyer Company";
+
+const AddressLine1 = document.getElementById('addressLine1');
+AddressLine1.innerHTML = "Apartment number, Building Name,";
+
+const AddressLine2 = document.getElementById('addressLine2');
+AddressLine2.innerHTML = "Society name, Area,";
+
+const AddressLine3 = document.getElementById('addressLine3');
+AddressLine3.innerHTML = "City - 000 000";
+
+const OrderID = document.getElementById('orderID');
+OrderID.innerHTML = "AZ12345678";
+
+const DateHolder = document.getElementById('date');
+DateHolder.innerHTML = "dd/mm/yyyy";
+
+const Terms = document.getElementById('terms');
+Terms.innerHTML = "*These Terms will be governed by and interpreted in accordance " + 
+                "with the laws of the State of Country, and you submit to the " + 
+                "non-exclusive jurisdiction of the state and federal courts located " +
+                "in Country for the resolution of any disputes.";
+
+const CompanyNameForFooter = document.getElementById('companyNameForFooter');
+CompanyNameForFooter.innerHTML = "For Name of the Company";
